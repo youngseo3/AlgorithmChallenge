baba6f522d0ad2458ef942ac6ba94dafca013448
영서 <116789976+youngseo3@users.noreply.github.com>
2024-04-12 21:17:22 +0900

    [Bronze III] Title: ACM 호텔, Time: 260 ms, Memory: 18188 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+// ACM 호텔
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = Integer.parseInt(sc.nextLine());
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(sc.nextLine());
+
+            int H = Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+
+            System.out.println(calcRoomNum(H, W, N));
+        }
+    }
+    public static String calcRoomNum(int H, int W, int N) {
+        int Y = N % H;
+        int X = N / H+1;
+        if (Y == 0) {
+            X --;
+            Y = H;
+        }
+        return String.format("%d%02d", Y, X);
+    }
+}
