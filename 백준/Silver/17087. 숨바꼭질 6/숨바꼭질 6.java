60d156dc895ab0c32bcc6638292a3457a178ea6c
영서 <116789976+youngseo3@users.noreply.github.com>
2025-01-16 16:02:02 +0900

    [Silver II] Title: 숨바꼭질 6, Time: 300 ms, Memory: 26080 KB -BaekjoonHub

--- 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.java
+++ 백준/Silver/17087. 숨바꼭질 6/숨바꼭질 6.java
@@ -8,7 +8,6 @@ public class Main {
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int min = Integer.MAX_VALUE;
         
         int N = Integer.parseInt(st.nextToken());
         int S = Integer.parseInt(st.nextToken());
