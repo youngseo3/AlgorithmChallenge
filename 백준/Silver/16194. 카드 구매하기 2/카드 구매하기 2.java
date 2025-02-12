95f31df1d2d13dd6003e9d95f0503023884948b4
영서 <116789976+youngseo3@users.noreply.github.com>
2025-02-12 23:06:55 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 124 ms, Memory: 14508 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] dp = new int[N + 1];
+
+        int[] p = new int[N + 1];
+        for(int i = 1; i < N + 1; i++) {
+        	p[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        for(int i = 1; i < N + 1; i++) {
+        	dp[i] = p[i];
+        	
+        	for(int j = 1; j <= i; j++) {
+        		dp[i] = Math.min(p[j] + dp[i - j], dp[i]);        	
+        	}
+        }
+        
+        System.out.println(dp[N]);
+	}
+	
+
+}
