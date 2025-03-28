8f3e903e8ba9617732d199c4c4fb76c86e131610
영서 <116789976+youngseo3@users.noreply.github.com>
2025-03-28 15:20:18 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -4,37 +4,25 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         
-        int divCnt = calculateDivCnt(N);
-        int result = 0;
-        int numCnt = 9;
-        int S = 0;
+        long result = 0;
+        int digit = 1;   // 자릿수
+        int start = 1;   // 자릿수가 바뀌는 시작 숫자
         
-        for(int i = 1; i <= divCnt; i++) {
-        	result += numCnt * i;
-        	S += numCnt;
-        	numCnt *= 10;
-        }
-        
-        if(N - S > 0) {
-        	result += (N - S) * (divCnt + 1);
+        while (start <= N) {
+            // 현재 자릿수의 마지막 숫자 계산 (N보다 작거나 같은 값)
+            int end = Math.min(N, start * 10 - 1);
+            
+            // 현재 자릿수에 해당하는 숫자의 개수 * 자릿수를 결과에 더함
+            result += (long) (end - start + 1) * digit;
+            
+            start *= 10;  // 다음 자릿수의 시작 숫자로 업데이트
+            digit++;      // 자릿수 증가
         }
         
         System.out.println(result);
-	}
-	
-	static int calculateDivCnt(int N) {
-        int divCnt = 0;
-
-		while(N / 10 > 0) {
-        	N /= 10;
-        	divCnt++;
-        }
-		
-		return divCnt;
-	}
-
+    }
 }
