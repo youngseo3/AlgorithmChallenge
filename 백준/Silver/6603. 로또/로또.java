960c1889020e80715c253d2cd9d2f35d5a56bb28
영서 <116789976+youngseo3@users.noreply.github.com>
2025-04-15 19:11:28 +0900

    [Silver II] Title: 로또, Time: 112 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -4,14 +4,12 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int k;
+    static int[] arr;
+    static int[] selected;
+    static StringBuilder sb = new StringBuilder();
 
-	static int k;
-	static int[] arr;
-	static int[] selected;
-	static boolean[] visited;
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         while(true) {
@@ -19,15 +17,14 @@ public class Main {
             k = Integer.parseInt(st.nextToken());
             
             if(k == 0) {
-            	break;
+                break;
             }
             
             arr = new int[k];
-            visited = new boolean[6];
             selected = new int[6];
             
             for(int j = 0; j < k; j++) {
-            	arr[j] = Integer.parseInt(st.nextToken());
+                arr[j] = Integer.parseInt(st.nextToken());
             }
             
             dfs(0, 0);
@@ -35,27 +32,22 @@ public class Main {
         }
         
         System.out.println(sb);
-	}
-	
-	static void dfs(int at, int depth) {
-		if(depth == 6) {
-			for (int i = 0; i < depth; i++) {
-				sb.append(selected[i] + " ");
-			}
-			sb.append("\n");
-			return;
-		}
-		
-		for (int i = at; i < k; i++) {
-			if(!visited[depth]) {
-				visited[depth] = true;
-				selected[depth] = arr[i];
-				
-				dfs(i + 1, depth + 1);
-				
-				visited[depth] = false;
-			}
-		}
-	}
-
+    }
+    
+    static void dfs(int start, int depth) {
+        // 6개의 수를 모두 선택했을 때
+        if(depth == 6) {
+            for (int i = 0; i < 6; i++) {
+                sb.append(selected[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        
+        // 남은 숫자들 중에서 선택
+        for (int i = start; i < k; i++) {
+            selected[depth] = arr[i];
+            dfs(i + 1, depth + 1);
+        }
+    }
 }
