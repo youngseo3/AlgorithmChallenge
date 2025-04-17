4a124438f575c73d7aa3c8698bb565e04a9d8d2a
영서 <116789976+youngseo3@users.noreply.github.com>
2025-04-18 02:11:47 +0900

    [Gold V] Title: 링크와 스타트, Time: 472 ms, Memory: 21224 KB -BaekjoonHub

--- 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
@@ -1,49 +1,75 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N, result;		
-    static int[][] player;		
-    static boolean[] visit;		
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        player = new int[N][N];
 
-        for(int i = 0; i<N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int j = 0; j<N; j++){
-                player[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-		
-        result = Integer.MAX_VALUE;
-        visit = new boolean[N];
-        solve(0);
-        System.out.println(result);
-    }
+	static int N, t;
+	static boolean[] check;
+	static int[][] board;
 	
-    static void solve(int cnt){
-        if(cnt == N){
-            int start = 0;
-            int link = 0;
-            for(int i = 0; i<N; i++){
-                for(int j = i+1; j<N; j++){
-                    if(visit[i] != visit[j]) continue;
-                    if (visit[i])
-                        start += player[i][j] + player[j][i];
-                    else
-                        link += player[i][j] + player[j][i];
-                }
-            }
-            int diff = Math.abs(start - link);
-            if(diff < result) result = diff;
-
-            return;
+	static int MIN_RESULT = Integer.MAX_VALUE;
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        t = 0;
+        N = Integer.parseInt(br.readLine());
+        
+        check = new boolean[N];
+        board = new int[N][N];
+        
+        for(int i = 0; i < N; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	
+        	for(int j = 0; j < N; j++) {
+            	board[i][j] = Integer.parseInt(st.nextToken());
+        	}
         }
-        visit[cnt] = true;
-        solve(cnt+1);
-        visit[cnt] = false;
-        solve(cnt+1);
+        
+        for(t = 1; t < N; t++) {
+            combination(0, 0);
+        }
+        
+        System.out.println(MIN_RESULT);
+	}
+    
+    static void combination(int start, int depth) {
+    	if(depth == t) {
+        	MIN_RESULT = Math.min(MIN_RESULT, getResult());
+        	
+        	if(MIN_RESULT == 0) {
+        		System.out.println(MIN_RESULT);		
+        		System.exit(0);			
+        	}			
+        	return;
+    	} 
+    	
+    	for(int i = start; i < N; i++) {
+    		if(!check[i]) {
+    			check[i] = true;
+    			combination(i + 1, depth + 1);
+    			check[i] = false;
+    		}
+    	}
     }
+    
+    static int getResult() {
+    	int start = 0;
+    	int link = 0;
+    	
+    	for(int i = 0; i < N; i++) {
+    		for(int j = 0; j < N; j++) {
+    			if(check[i] && check[j]) {
+    				start += board[i][j];
+    			} else if(!check[i] && !check[j]) {
+    				link += board[i][j];
+    			}
+    		}
+    	}
+    	
+        return Math.abs(start - link);
+    }
+
 }
