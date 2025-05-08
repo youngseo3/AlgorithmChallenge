fda1e87248e1534b4aff9d1360e4997c461819ef
영서 <116789976+youngseo3@users.noreply.github.com>
2025-05-08 19:59:02 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 75 ms, Memory: 25,216 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -2,6 +2,8 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Solution {
+    private static final int MAX_HEIGHT = 100; // 상자 높이의 최대값
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -10,13 +12,17 @@ public class Solution {
             int dumpCount = Integer.parseInt(br.readLine());
             StringTokenizer st = new StringTokenizer(br.readLine());
 
-            int[] height = new int[100];
+            // 높이별 상자 개수를 저장하는 배열
+            int[] heightCount = new int[MAX_HEIGHT + 1];
+            
+            // 입력 받으면서 높이별 상자 개수 계산
             for (int i = 0; i < 100; i++) {
-                height[i] = Integer.parseInt(st.nextToken());
+                int h = Integer.parseInt(st.nextToken());
+                heightCount[h]++;
             }
-
+            
             // 평탄화 작업 수행
-            int result = flatten(height, dumpCount);
+            int result = flattenOptimized(heightCount, dumpCount);
             
             sb.append("#").append(tc).append(" ").append(result).append("\n");
         }
@@ -24,46 +30,34 @@ public class Solution {
         System.out.println(sb);
     }
 
-    private static int flatten(int[] height, int dumpCount) {
+    private static int flattenOptimized(int[] heightCount, int dumpCount) {
+        int minHeight = 1;
+        int maxHeight = 100;
+        
+        // 실제 최소/최대 높이 찾기
+        while (heightCount[minHeight] == 0) minHeight++;
+        while (heightCount[maxHeight] == 0) maxHeight--;
+        
+        // 평탄화 작업
         for (int i = 0; i < dumpCount; i++) {
-            // 최대값과 최소값의 인덱스 찾기
-            int maxIdx = findMaxIndex(height);
-            int minIdx = findMinIndex(height);
-            
             // 이미 평탄화가 완료되었다면 중단
-            if (height[maxIdx] - height[minIdx] <= 1) {
-                break;
-            }
+            if (maxHeight - minHeight <= 1) break;
             
-            // 덤프 수행
-            height[maxIdx]--;
-            height[minIdx]++;
+            // 최고 높이 감소, 최저 높이 증가
+            heightCount[maxHeight]--;
+            heightCount[maxHeight - 1]++;
+            heightCount[minHeight]--;
+            heightCount[minHeight + 1]++;
+            
+            // 최고/최저 높이 갱신
+            if (heightCount[maxHeight] == 0) maxHeight--;
+            if (heightCount[minHeight] == 0) minHeight++;
         }
         
         // 최종 높이 차이 계산
-        int maxIdx = findMaxIndex(height);
-        int minIdx = findMinIndex(height);
+        while (heightCount[minHeight] == 0) minHeight++;
+        while (heightCount[maxHeight] == 0) maxHeight--;
         
-        return height[maxIdx] - height[minIdx];
-    }
-    
-    private static int findMaxIndex(int[] arr) {
-        int maxIdx = 0;
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > arr[maxIdx]) {
-                maxIdx = i;
-            }
-        }
-        return maxIdx;
-    }
-    
-    private static int findMinIndex(int[] arr) {
-        int minIdx = 0;
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < arr[minIdx]) {
-                minIdx = i;
-            }
-        }
-        return minIdx;
+        return maxHeight - minHeight;
     }
 }
