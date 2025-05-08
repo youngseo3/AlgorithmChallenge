5f763556076cd124794b379dbecaa8d0902409ac
영서 <116789976+youngseo3@users.noreply.github.com>
2025-05-08 19:58:38 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 88 ms, Memory: 26,624 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -1,38 +1,69 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
-	
-	static int[] height;
-	static int dumpCount;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-				
-		for(int tc = 1; tc <= 10; tc++) {
-			dumpCount = Integer.parseInt(br.readLine());
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			
-			height = new int[100];
-			for(int i = 0; i < 100; i++) {
-				height[i] = Integer.parseInt(st.nextToken());;
-			}
-			
-			for(int i = 0; i < dumpCount; i++) {
-				Arrays.sort(height);
-				height[0]++;
-				height[99]--;
-			}
-			
-			Arrays.sort(height);
-			int result = height[99] - height[0];
-			sb.append("#" + tc + " ").append(result + "\n");
-		}
-		
-		System.out.println(sb);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-	}
+        for (int tc = 1; tc <= 10; tc++) {
+            int dumpCount = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
+            int[] height = new int[100];
+            for (int i = 0; i < 100; i++) {
+                height[i] = Integer.parseInt(st.nextToken());
+            }
+
+            // 평탄화 작업 수행
+            int result = flatten(height, dumpCount);
+            
+            sb.append("#").append(tc).append(" ").append(result).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static int flatten(int[] height, int dumpCount) {
+        for (int i = 0; i < dumpCount; i++) {
+            // 최대값과 최소값의 인덱스 찾기
+            int maxIdx = findMaxIndex(height);
+            int minIdx = findMinIndex(height);
+            
+            // 이미 평탄화가 완료되었다면 중단
+            if (height[maxIdx] - height[minIdx] <= 1) {
+                break;
+            }
+            
+            // 덤프 수행
+            height[maxIdx]--;
+            height[minIdx]++;
+        }
+        
+        // 최종 높이 차이 계산
+        int maxIdx = findMaxIndex(height);
+        int minIdx = findMinIndex(height);
+        
+        return height[maxIdx] - height[minIdx];
+    }
+    
+    private static int findMaxIndex(int[] arr) {
+        int maxIdx = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[maxIdx]) {
+                maxIdx = i;
+            }
+        }
+        return maxIdx;
+    }
+    
+    private static int findMinIndex(int[] arr) {
+        int minIdx = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[minIdx]) {
+                minIdx = i;
+            }
+        }
+        return minIdx;
+    }
 }
