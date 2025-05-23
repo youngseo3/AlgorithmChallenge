59447d85b78d912b3927f6f47d2ad46901c641f4
영서 <116789976+youngseo3@users.noreply.github.com>
2025-05-23 17:33:59 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 73 ms, Memory: 26,240 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.java
@@ -1,36 +1,41 @@
 import java.io.*;
+import java.util.*;
+
 public class Solution {
 
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
 		
+		int T = Integer.parseInt(br.readLine());
 		for(int tc = 1; tc <= T; tc++) {
 			int N = Integer.parseInt(br.readLine());
+			int temp = 0;
 			boolean[] visited = new boolean[10];
-			boolean sleep = false;
-			int k = 1;
-			while(!sleep) {
-				int temp = N * k++;
-				while(temp != 0) {
-					int remain = temp % 10;
-					visited[remain] = true;
-					temp /= 10;
+
+			while(true) {
+				int cnt = 0;
+				temp += N;
+				
+				String str = String.valueOf(temp);
+				for(int i = 0; i < str.length(); i++) {
+					visited[Integer.parseInt(String.valueOf(str.charAt(i)))] = true;
 				}
 				
-				sleep = true;
 				for(int i = 0; i < 10; i++) {
-					if(!visited[i]) {
-						sleep = false;
-						break;
+					if(visited[i]) {
+						cnt++;
 					}
+				}
+				
+				if(cnt == 10) {
+					break;
 				}
 			}
 			
-			sb.append("#" + tc + " ").append((k - 1) * N + "\n");
+			sb.append("#" + tc + " ").append(temp + "\n");
 		}
-		
 		System.out.println(sb);
 	}
 
