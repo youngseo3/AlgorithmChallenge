7fcf37aa1bc5c937255ea72e7e9201ad2b78a3f2
영서 <116789976+youngseo3@users.noreply.github.com>
2025-08-05 15:27:47 +0900

    [Gold IV] Title: 알파벳, Time: 844 ms, Memory: 12296 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -7,7 +7,7 @@ public class Main {
 	static int[] dx = {-1, 1, 0, 0};
 	static int[] dy = {0, 0, -1, 1};
 	static char[][] arr;
-	static List<Character> list = new ArrayList<>();
+	static boolean[] alphabet;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,6 +16,7 @@ public class Main {
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		arr = new char[R][C];
+		alphabet = new boolean[26];
 		
 		for(int i = 0; i < R; i++) {
 			String line = br.readLine();
@@ -25,38 +26,32 @@ public class Main {
 			}
 		}
 		
-		list.add(arr[0][0]);
+		alphabet[arr[0][0] - 'A'] = true;
 		dfs(0, 0, 1);
 		
 		System.out.println(MAX);
 	}
 
-private static void dfs(int x, int y, int cnt) {
-    MAX = Math.max(MAX, cnt);  // 현재 위치에서 MAX 갱신
-    
-    for(int i = 0; i < 4; i++) {
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-        
-        if(nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
-        
-        if(check(nx, ny)) { // 갈 수 있는 경우만
-            list.add(arr[nx][ny]);
-            dfs(nx, ny, cnt + 1);
-            list.remove(list.size() - 1);  // 백트래킹
-        }
-    }
-}
-
-	private static boolean check(int x, int y) {
-		for(char c: list) {
-			if(c == arr[x][y]) {
-				return false;
+	private static void dfs(int x, int y, int cnt) {
+		MAX = Math.max(MAX, cnt);
+		
+		for(int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			
+			if(nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
+			
+			int idx = arr[nx][ny] - 'A';
+			
+			// 갈 수 있는 경우
+			if(!alphabet[idx]) {
+				alphabet[idx] = true;
+				dfs(nx, ny, cnt + 1);
+				alphabet[idx] = false;
 			}
+			
 		}
 		
-		return true;
 	}
-	
 	
 }
