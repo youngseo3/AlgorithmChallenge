0d57d9ac79e8ce612e6d9c79d918d74863eea3fd
영서 <116789976+youngseo3@users.noreply.github.com>
2025-08-07 01:22:15 +0900

    [Silver II] Title: 섬의 개수, Time: 156 ms, Memory: 16544 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -2,81 +2,70 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
+	
 	static int w, h;
-	static int result;
-	static int[][] map;
-	static int[] r = {-1, 1, 0, 0, -1, -1, 1, 1};
-	static int[] c = {0, 0, -1, 1, -1, 1, -1, 1};
+	static int[] dx = {-1, 1, 0, 0, -1, -1, 1, 1};
+	static int[] dy = {0, 0, -1, 1, -1, 1, -1, 1};
+	
+	static int[][] island;
+	static boolean[][] visited;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
-		String str = "";
-		while(!(str = br.readLine()).equals("0 0")) {
-			st = new StringTokenizer(str);
+		while(true) {
+			st = new StringTokenizer(br.readLine());
 			w = Integer.parseInt(st.nextToken());
 			h = Integer.parseInt(st.nextToken());
-			result = 0;
-			map = new int[h][w];
+			
+			if(w == 0 && h == 0) break;
+
+			island = new int[h][w];
+			visited = new boolean[h][w];
+			
 			
 			for(int i = 0; i < h; i++) {
 				st = new StringTokenizer(br.readLine());
 				
 				for(int j = 0; j < w; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
+					island[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
 			
-			for(int i = 0; i < h; i++) {			
+			int cnt = 0;
+			for(int i = 0; i < h; i++) {
 				for(int j = 0; j < w; j++) {
-					if(map[i][j] == 1) {
-						bfs(j, i);
-						result++;
+					if(island[i][j] == 1 && !visited[i][j]) {
+						dfs(i, j);
+						cnt++;
 					}
 				}
 			}
 			
-			sb.append(result).append("\n");
+			sb.append(cnt + "\n");
 		}
 		
 		System.out.println(sb);
-		
 	}
-	
-	static void bfs(int x, int y) {
-		Queue<Point> q = new LinkedList<>();
-		q.add(new Point(x, y));
+
+	private static void dfs(int x, int y) {
+		visited[x][y] = true;
 		
-		map[y][x] = 0;
-		
-		while(!q.isEmpty()) {
-			Point p = q.poll();
+		for(int i = 0; i < 8; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
 			
-			for(int i = 0; i < 8; i++) {
-				int a = p.y + r[i];
-				int b = p.x + c[i];
-				
-				if((a >= 0 && a < h) && (b >= 0 && b < w) && map[a][b] == 1) {
-					q.add(new Point(b, a));
-					map[a][b] = 0;
-				}
-			}
+			if(nx < 0 || nx >= h || ny < 0 || ny >= w) continue;
+			
+			if(visited[nx][ny]) continue;
+			
+			if(island[nx][ny] == 0) continue;
+			
+			dfs(nx, ny);
 		}
 		
 	}
 
-}
-
-class Point {
-	int x;
-	int y;
-	
-	public Point(int x, int y) {
-		this.x = x;
-		this.y = y;
-		
-	}
 }
