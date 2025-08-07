06280c9443351718aab614376b80837e9dee6e28
영서 <116789976+youngseo3@users.noreply.github.com>
2025-08-07 15:32:31 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10532 ms, Memory: 303348 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -3,71 +3,66 @@ import java.util.*;
 
 public class Main {
 
-    private static int[] dx = {1, -1, 0, 0, 1, 1, -1, -1};
-    private static int[] dy = {0, 0, 1, -1, 1, -1, 1, -1};
+	static int N, M, max;
+	static int[] cnt;
+	static boolean[] visited;
+	static List<Integer>[] computer;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		cnt = new int[N + 1];
+		computer = new ArrayList[N + 1];
+		
+		for(int i = 1; i <= N; i++) {
+			computer[i] = new ArrayList<>();
+		}
+		
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			computer[b].add(a);
+		}
+		
+		for(int i = 1; i <= N; i++) {
+			visited = new boolean[N + 1];
+			bfs(i);
+		}
+		
+		for(int i = 1; i <= N; i++) {
+			if(cnt[i] == max) {
+				sb.append(i).append(" ");
+			}
+		}
+		
+		System.out.println(sb);
+	}
+	
+	static void bfs(int start) {
+		Queue<Integer> q = new ArrayDeque<>();
+		visited[start] = true;
+		q.add(start);
+		
+		while(!q.isEmpty()) {
+			int B = q.poll();
+			
+			for(int A: computer[B]) {
+				if(!visited[A]) {
+					visited[A] = true;
+					q.add(A);
+					cnt[start]++;
+				}
+			}
+		}
+		
+		max = Math.max(max, cnt[start]);
+	}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        List<List<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i <= N; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            graph.get(B).add(A);
-        }
-
-        int max = 0;
-        int[] counts = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            counts[i] = search(i, N, graph);
-            max = Math.max(max, counts[i]);
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= N; i++) {
-            if (counts[i] == max) {
-                sb.append(i).append(" ");
-            }
-        }
-
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-
-    private static int search(int start, int N, List<List<Integer>> graph) {
-        boolean[] visited = new boolean[N + 1];
-        Queue<Integer> queue = new ArrayDeque<>();
-
-        queue.add(start);
-        visited[start] = true;
-        int count = 1;
-
-        while (!queue.isEmpty()) {
-            int curr = queue.poll();
-
-            for (int next : graph.get(curr)) {
-                if (visited[next]) {
-                    continue;
-                }
-                visited[next] = true;
-                queue.add(next);
-                count++;
-            }
-        }
-
-        return count;
-    }
 }
