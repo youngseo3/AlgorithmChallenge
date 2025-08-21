85885a2376e44f666cb418e7a73dc696210bd229
영서 <116789976+youngseo3@users.noreply.github.com>
2025-08-21 19:49:10 +0900

    [D6] Title: [S/W 문제해결 응용] 10일차 - 작업순서, Time: 107 ms, Memory: 27,904 KB -BaekjoonHub

--- SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
+++ SWEA/D6/1267. ［S／W 문제해결 응용］ 10일차 － 작업순서/［S／W 문제해결 응용］ 10일차 － 작업순서.java
@@ -3,70 +3,53 @@ import java.util.*;
 
 public class Solution {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		
-		for(int tc = 1; tc <= 10; tc++) {
-			st = new StringTokenizer(br.readLine());
-			
-			int V = Integer.parseInt(st.nextToken());
-			int E = Integer.parseInt(st.nextToken());
-			
-			int[] inDegree = new int[V + 1];
-			boolean[] visited = new boolean[V + 1];
-			Queue<Integer> q = new ArrayDeque<>();
-			
-			List<List<Integer>> edges = new ArrayList<>();
-			for(int i = 0; i <= V; i++) {
-				edges.add(new ArrayList<>());
-			}
-			
-			st = new StringTokenizer(br.readLine());
-			for(int i = 0; i < E; i++) {
-				int a = Integer.parseInt(st.nextToken());
-				int b = Integer.parseInt(st.nextToken());
-				
-				edges.get(a).add(b);
-				inDegree[b]++;
-			}
-			
-//			System.out.println(Arrays.toString(inDegree));
-			
-			for(int i = 1; i <= V; i++) {
-				if(inDegree[i] == 0) {
-					visited[i] = true;
-					q.offer(i);
-				}
-			}
-			
-			Queue<Integer> T = new ArrayDeque<>();
-			
-			while(!q.isEmpty()) {
-				int current = q.poll();
-				T.offer(current);
-				
-				for(int next: edges.get(current)) {
-					if(!visited[next]) {
-						inDegree[next]--;
-					}
-					
-					if(inDegree[next] == 0) {
-						visited[next] = true;
-						q.offer(next);
-					}
-				}
-			}
-			
-			sb.append("#" + tc + " ");
-			while(!T.isEmpty()) {
-				sb.append(T.poll() + " ");
-			}
-			sb.append("\n");
-		}
-		
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
+        for (int tc = 1; tc <= 10; tc++) {
+            st = new StringTokenizer(br.readLine());
+            int V = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+
+            int[] inDegree = new int[V + 1];
+            List<List<Integer>> edges = new ArrayList<>();
+            for (int i = 0; i <= V; i++) {
+                edges.add(new ArrayList<>());
+            }
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < E; i++) {
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                edges.get(a).add(b);
+                inDegree[b]++;
+            }
+
+            Queue<Integer> q = new ArrayDeque<>();
+            for (int i = 1; i <= V; i++) {
+                if (inDegree[i] == 0) {
+                    q.offer(i);
+                }
+            }
+
+            sb.append("#").append(tc).append(" ");
+
+            while (!q.isEmpty()) {
+                int current = q.poll();
+                sb.append(current).append(" ");
+
+                for (int next : edges.get(current)) {
+                    inDegree[next]--;
+                    if (inDegree[next] == 0) {
+                        q.offer(next);
+                    }
+                }
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
 }
