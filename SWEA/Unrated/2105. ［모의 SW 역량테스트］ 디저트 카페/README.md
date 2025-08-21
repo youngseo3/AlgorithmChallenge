f87497ab8b91bbe5bfe7605ed62c74cc39f33d9b
영서 <116789976+youngseo3@users.noreply.github.com>
2025-08-22 00:58:09 +0900

    [Unrated] Title: [모의 SW 역량테스트] 디저트 카페, Time: 148 ms, Memory: 32,640 KB -BaekjoonHub

---
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/README.md
@@ -0,0 +1,15 @@
+# [Unrated] [모의 SW 역량테스트] 디저트 카페 - 2105 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5VwAr6APYDFAWu) 
+
+### 성능 요약
+
+메모리: 32,640 KB, 시간: 148 ms, 코드길이: 1,713 Bytes
+
+### 제출 일자
+
+2025-08-22 00:58
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	static int N, max;
+	static int[] dx = {1, 1, -1, -1};
+	static int[] dy = {1, -1, -1, 1};
+	static int[][] cafe;
+	static boolean[][] visited;
+	static boolean[] dessert;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine());
+			max = 0;
+			cafe = new int[N][N];
+			
+			for(int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				
+				for(int j = 0; j < N; j++) {
+					cafe[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			for(int i = 0; i < N - 2; i++) {
+				for(int j = 1; j < N - 1; j++) {
+					visited = new boolean[N][N];
+					dessert = new boolean[101];
+					visited[i][j] = true;
+					dessert[cafe[i][j]] = true;
+					dfs(1, i, j, i, j, 0);
+				}
+			}
+			
+			if(max == 0) max = - 1;
+			sb.append("#" + tc + " ").append(max).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+
+	private static void dfs(int cnt, int x, int y, int initX, int initY, int dir) {
+		for(int i = dir; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			
+			if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+			
+			if(nx == initX && ny == initY && cnt >= 4) {
+				max = Math.max(max, cnt);
+				return;
+			}
+			
+			if(!visited[nx][ny] && !dessert[cafe[nx][ny]]) {
+				visited[nx][ny] = true;
+				dessert[cafe[nx][ny]] = true;
+				
+				dfs(cnt + 1, nx, ny, initX, initY, i);
+				
+				visited[nx][ny] = false;
+				dessert[cafe[nx][ny]] = false;
+			}
+		}
+	}
+
+}
