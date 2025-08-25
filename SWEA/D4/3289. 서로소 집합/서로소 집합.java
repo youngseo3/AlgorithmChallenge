8e94350c6c7981b04cc4c0ec0170dcd5509a09d1
영서 <116789976+youngseo3@users.noreply.github.com>
2025-08-25 16:15:31 +0900

    [D4] Title: 서로소 집합, Time: 468 ms, Memory: 116,772 KB -BaekjoonHub

--- SWEA/D4/3289. 서로소 집합/서로소 집합.java
+++ SWEA/D4/3289. 서로소 집합/서로소 집합.java
@@ -3,7 +3,8 @@ import java.util.*;
 
 public class Solution {
 
-	static int[] parents; 
+	static int[] parents;
+	static int[] rank; //  1. 랭크를 저장할 배열 선언
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,11 +16,14 @@ public class Solution {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int n = Integer.parseInt(st.nextToken());
 			int m = Integer.parseInt(st.nextToken());
+
 			parents = new int[n + 1];
+			rank = new int[n + 1]; //  2. 랭크 배열 생성
 			sb.append("#" + tc + " ");
 			
 		    for (int i = 1; i <= n; i++) {
-		        parents[i] = i; // 모든 원소가 자기 자신을 부모로 설정
+		        parents[i] = i;
+		        // rank[i] = 0; // Java에서 int 배열은 자동으로 0으로 초기화되므로 생략 가능
 		    }
 			
 			for(int i = 0; i < m; i++) {
@@ -50,18 +54,26 @@ public class Solution {
 		if(x != parents[x]) {
 			parents[x] = find(parents[x]);
 		}
-		
 		return parents[x];
 	}
 
+	//  3. Union-by-Rank 로직이 적용된 union 메서드
 	private static boolean union(int a, int b) {
 		int rootA = find(a);
-		int	rootB = find(b);
+		int rootB = find(b);
 		
 		if(rootA == rootB) return false;
 		
-		parents[rootB] = rootA;
+		// 랭크 비교
+		if (rank[rootA] < rank[rootB]) {
+			parents[rootA] = rootB;
+		} else if (rank[rootA] > rank[rootB]) {
+			parents[rootB] = rootA;
+		} else { // 랭크가 같은 경우
+			parents[rootB] = rootA; // 한쪽을 다른 쪽에 붙이고
+			rank[rootA]++;         // 루트가 된 노드의 랭크를 1 증가
+		}
+		
 		return true;
 	}
-
 }
