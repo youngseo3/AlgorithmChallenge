1314861f7bf5ed688dbbdbdd36d1ca380c9e858c
영서 <116789976+youngseo3@users.noreply.github.com>
2025-09-05 09:45:32 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 88 ms, Memory: 11912 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -1,40 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int[] A = new int[n + 1];
-		int[] dp = new int[n + 1];
+		int N = Integer.parseInt(br.readLine());
 
-		for(int i = 1; i <= n; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] nums = new int[N];
+		for(int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		dp[1] = 1;
-		for(int i = 2; i <= n; i++) {	
+
+		int[] dp = new int[N];
+
+		for(int i = 0; i < N; i++) {
 			dp[i] = 1;
-			
-			for(int j = 1; j < i; j++) {
-				if(A[i] < A[j]) {
+
+			for(int j = 0; j < i; j++) {
+				if(nums[j] > nums[i]) {
 					dp[i] = Math.max(dp[i], dp[j] + 1);
 				}
-			}	
+			}
 		}
-		
-		int max = 0;
 
-        for(int i = 1; i <= n; i++) {
-            max = Math.max(dp[i], max);
-        }
-        
-        System.out.println(max);
+		// dp 배열의 값들 중 최댓값을 찾아야 함
+		int max = 0;
+		for (int length : dp) {
+			if (length > max) {
+				max = length;
+			}
+		}
+
+		System.out.println(max);
 	}
 
 }
