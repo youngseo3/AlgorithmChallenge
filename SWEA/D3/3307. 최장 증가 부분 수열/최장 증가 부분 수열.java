4c8231921b61ba5dece28f986319de22bfec575e
영서 <116789976+youngseo3@users.noreply.github.com>
2025-09-09 12:09:39 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 101 ms, Memory: 28,416 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
@@ -3,6 +3,8 @@ import java.util.*;
 
 public class Solution {
 
+	static List<Integer> lis;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
@@ -10,31 +12,44 @@ public class Solution {
 		
 		int T = Integer.parseInt(br.readLine());
 		for(int tc = 1; tc <= T; tc++) {
+			lis = new ArrayList<>();
+			
 			int N = Integer.parseInt(br.readLine());
 			st = new StringTokenizer(br.readLine());
 			
-			int[] A = new int[N];
 			for(int i = 0; i < N; i++) {
-				A[i] = Integer.parseInt(st.nextToken());
-			}
-			
-			int max = 1;
-			int[] dp = new int[N];
-			for(int i = 0; i < N; i++) {
-				dp[i] = 1;
+				int num = Integer.parseInt(st.nextToken());
 				
-				for(int j = 0; j < i; j++) {
-					if(A[i] > A[j]) {
-						dp[i] = Math.max(dp[i], dp[j] + 1);
-						max = Math.max(dp[i], max);
-					}
+				if(lis.size() == 0 || lis.get(lis.size() - 1) < num) {
+					lis.add(num);
+				}
+				else {
+					lis.set(lowerBound(num), num);
 				}
 			}
 			
-			sb.append("#" + tc + " ").append(max + "\n");
+			sb.append("#" + tc + " ").append(lis.size() + "\n");
 		}
 		
 		System.out.println(sb);
+	}
+
+	private static int lowerBound(int target) {
+		int left = 0;
+		int right = lis.size();
+		
+		while(left < right) {
+			int mid = (left + right) / 2;
+			
+			if(lis.get(mid) < target) {
+				left = mid + 1;
+			}
+			else {
+				right = mid;
+			}
+		}
+		
+		return right;
 	}
 
 }
