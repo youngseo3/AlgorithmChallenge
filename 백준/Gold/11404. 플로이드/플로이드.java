3355b0d1d5405be60b2a701d209eeb445b7dce5a
영서 <116789976+youngseo3@users.noreply.github.com>
2025-09-10 16:32:06 +0900

    [Gold IV] Title: 플로이드, Time: 352 ms, Memory: 49384 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,20 +1,25 @@
 import java.io.*;
 import java.util.*;
-	
+
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
 		int n = Integer.parseInt(br.readLine());
-		int m = Integer.parseInt(br.readLine());
-		int[][] dist = new int[n + 1][n + 1];
+		int m = Integer.parseInt(br.readLine()); 
+		
+		// 1번부터  k까지 경유할 때 i에서 j로 가는데 최단 거리
+		int[][][] dp = new int[n + 1][n + 1][n + 1];
 		for(int i = 1; i <= n; i++) {
-			Arrays.fill(dist[i], Integer.MAX_VALUE);
-			dist[i][i] = 0;
+			for(int j = 1; j <= n; j++) {
+				Arrays.fill(dp[i][j], Integer.MAX_VALUE);
+				if(i == j) {
+					Arrays.fill(dp[i][j], 0);
+				}
+			}
 		}
-
+		
 		for(int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
 			
@@ -22,30 +27,35 @@ public class Main {
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
 			
-			dist[a][b] = Math.min(dist[a][b], c);
+			dp[a][b][0] = Math.min(dp[a][b][0], c);
 		}
 		
 		for(int k = 1; k <= n; k++) {
 			for(int i = 1; i <= n; i++) {
 				for(int j = 1; j <= n; j++) {
-					if(dist[i][k] != Integer.MAX_VALUE && dist[k][j] != Integer.MAX_VALUE) {
-						dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+					if(dp[i][k][k - 1] != Integer.MAX_VALUE && dp[k][j][k - 1] != Integer.MAX_VALUE) {
+						dp[i][j][k] = Math.min(dp[i][j][k - 1], dp[i][k][k - 1] + dp[k][j][k - 1]);
+					}
+					else {
+						dp[i][j][k] = dp[i][j][k - 1];
 					}
 				}
 			}
 		}
 		
-		for(int i = 1; i <= n; i++) {
-			for(int j = 1; j <= n; j++) {
-				if(dist[i][j] == Integer.MAX_VALUE) {
-					dist[i][j] = 0;
-				}
-				
-				System.out.print(dist[i][j] + " ");
-			}
-			System.out.println();
-		}
-		
+		StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(dp[i][j][n] == Integer.MAX_VALUE) {
+                    sb.append("0 ");
+                } else {
+                    sb.append(dp[i][j][n] + " ");
+                }
+            }
+            sb.append("\n");
+        }
+        
+        System.out.print(sb);
 	}
 
 }
