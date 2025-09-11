012d4728524af8197fe1635664109aaca1fea8ef
영서 <116789976+youngseo3@users.noreply.github.com>
2025-09-11 11:39:51 +0900

    [Gold IV] Title: 치즈, Time: 96 ms, Memory: 13984 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.java
+++ 백준/Gold/2636. 치즈/치즈.java
@@ -2,10 +2,12 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int h, w, time, lastCheeseCount;
+
+	static int h, w, time;
 	static int[] dx = {-1, 1, 0, 0};
 	static int[] dy = {0, 0, -1, 1};
 	static int[][] map;
+	static int[][] times;
 	static boolean[][] visited;
 	
 	public static void main(String[] args) throws IOException {
@@ -15,6 +17,7 @@ public class Main {
 		h = Integer.parseInt(st.nextToken());
 		w = Integer.parseInt(st.nextToken());
 		map = new int[h][w];
+		times = new int[h][w];
 		visited = new boolean[h][w];
 		
 		for(int i = 0; i < h; i++) {
@@ -27,55 +30,93 @@ public class Main {
 		
 		solve();
 		
-		System.out.println(time);
-		System.out.println(lastCheeseCount);
-	}
-	
-	private static void solve() {
-		while(!checkCheese()) {
-			visited = new boolean[h][w];
-			bfs(0, 0);
-		}
-	}
-	
-	private static void bfs(int x, int y) {
-		time++;
-		
-		Queue<int[]> q = new ArrayDeque<>();
-		q.offer(new int[] {x, y});
-		visited[x][y] = true;
-		
-		List<int[]> toMelt = new ArrayList<>();
-				
-		while(!q.isEmpty()) {
-			int[] cxy = q.poll();
-			int cx = cxy[0];
-			int cy = cxy[1];
-			
-			for(int i = 0; i < 4; i++) {
-				int nx = cx + dx[i];
-				int ny = cy + dy[i];
-				
-				if(nx < 0 || nx >= h || ny < 0 || ny >= w) continue;
-				if(visited[nx][ny]) continue;
-				
-				visited[nx][ny] = true;
-				
-				if(map[nx][ny] == 0) {
-					q.offer(new int[] {nx, ny});
-				} else {
-					toMelt.add(new int[] {nx, ny});
-				}
+		int cnt = 0;
+		for(int i = 0; i < h; i++) {
+			for(int j = 0; j < w; j++) {
+				if(times[i][j] == time) cnt++;
 			}
 		}
 		
-		for(int[] cheese : toMelt) {
-			map[cheese[0]][cheese[1]] = 0;
+		System.out.println(time);
+		System.out.println(cnt);
+	}
+
+	private static void solve() {
+//		while(!checkCheese()) {
+//			for(int i = 0; i < h; i++) {
+//				for(int j = 0; j < w; j++) {
+//					if(map[i][j] == 0) {
+//						bfs(i, j);
+//					}
+//				}
+//			}
+//		}
+		for(int i = 0; i < h; i++) {
+			for(int j = 0; j < w; j++) {
+				if(map[i][j] == 0) {
+					bfs(i, j);
+				}
+			}
+		}
+	}
+
+	private static void bfs(int x, int y) {
+		boolean isStart = false;
+		
+		for(int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			
+			if(nx < 0 || nx >= h || ny < 0 || ny >= w) continue;
+			
+			if(map[nx][ny] == 1) {
+				isStart = true;
+			}
 		}
 		
-		lastCheeseCount = toMelt.size();
+		if(isStart) {
+			time++;
+
+			Queue<int[]> q = new ArrayDeque<>();
+			q.offer(new int[] {x, y});
+			
+			boolean[][] isZero = new boolean[h][w];
+					
+			while(!q.isEmpty()) {
+				int[] cxy = q.poll();
+				int cx = cxy[0];
+				int cy = cxy[1];
+				
+				isZero[cx][cy] = true;
+				
+				for(int i = 0; i < 4; i++) {
+					int nx = cx + dx[i];
+					int ny = cy + dy[i];
+					
+					if(nx < 0 || nx >= h || ny < 0 || ny >= w) continue;
+					
+					if(!visited[nx][ny] && map[nx][ny] == 1) {
+						visited[nx][ny] = true;
+					}
+					else if(!isZero[nx][ny] && map[nx][ny] == 0) {
+						q.offer(new int[] {nx, ny});
+                        isZero[nx][ny] = true;  // 또는 visited[nx][ny] = true;
+					}
+				}
+			}
+			
+			for(int i = 0; i < h; i++) {
+				for(int j = 0; j < w; j++) {
+					if(visited[i][j]) {
+						visited[i][j] = false;
+						map[i][j] = 0;
+						times[i][j] = time;
+					}
+				}
+			}
+		}
 	}
-	
+
 	private static boolean checkCheese() {
 		for(int i = 0; i < h; i++) {
 			for(int j = 0; j < w; j++) {
@@ -87,4 +128,5 @@ public class Main {
 		
 		return true;
 	}
+
 }
