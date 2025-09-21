7b2aaab6894bcecf8f0b5ebd1e207a33348cf2cb
영서 <116789976+youngseo3@users.noreply.github.com>
2025-09-21 16:14:07 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 604 ms, Memory: 113008 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -2,30 +2,30 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
+	
 	static int N, M;
 	static int[] dx = {-1, 1, 0, 0};
-	static int[] dy = {0, 0, -1, 1};
+	static int[] dy = {0, 0, -1, 1}; 
 	static int[][] map;
 	static boolean[][][] visited;
-
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
+		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		map = new int[N][M];
 		visited = new boolean[N][M][2];
-
+		
 		for(int i = 0; i < N; i++) {
 			String line = br.readLine();
-
+			
 			for(int j = 0; j < M; j++) {
 				map[i][j] = line.charAt(j) - '0';
 			}
 		}
-
+		
 		System.out.println(bfs());
 	}
 
@@ -33,31 +33,36 @@ public class Main {
 		Queue<Point> q = new ArrayDeque<>();
 		q.offer(new Point(0, 0, 1, 0));
 		visited[0][0][0] = true;
-
+		
 		while(!q.isEmpty()) {
-			Point p = q.poll();
-
-			if(p.x == N - 1 && p.y == M - 1) {
-				return p.cnt;
+			Point cur = q.poll();
+			int x = cur.x;
+			int y = cur.y;
+			int cnt = cur.cnt;
+			int isBreak = cur.isBreak;
+			
+			if(x == N - 1 && y == M - 1) {
+				return cnt;
 			}
 			
 			for(int i = 0; i < 4; i++) {
-				int nx = p.x + dx[i];
-				int ny = p.y + dy[i];
-
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				
 				if(nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
-
-				if(map[nx][ny] == 0 && !visited[nx][ny][p.isBreak]) {
-					visited[nx][ny][p.isBreak] = true;
-					q.offer(new Point(nx, ny, p.cnt + 1, p.isBreak));
-				} else if(map[nx][ny] == 1 && p.isBreak == 0) {
+				
+				if(map[nx][ny] == 0 && !visited[nx][ny][isBreak]) {
+					visited[nx][ny][isBreak] = true;
+					q.offer(new Point(nx, ny, cnt + 1, isBreak));
+				}
+				else if(map[nx][ny] == 1 && isBreak == 0) {
 					visited[nx][ny][1] = true;
-					q.offer(new Point(nx, ny, p.cnt + 1, 1));
+					q.offer(new Point(nx, ny, cnt + 1, 1));
 				}
 			}
 		}
 		
-		return -1;
+		return - 1;
 	}
 
 	static class Point {
@@ -65,7 +70,7 @@ public class Main {
 		int y;
 		int cnt;
 		int isBreak;
-
+		
 		public Point(int x, int y, int cnt, int isBreak) {
 			this.x = x;
 			this.y = y;
