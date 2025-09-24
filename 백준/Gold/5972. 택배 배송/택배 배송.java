fb5de189a2a19acc23a3b62c78d773d11ea7bf62
영서 <116789976+youngseo3@users.noreply.github.com>
2025-09-24 17:51:37 +0900

    [Gold V] Title: 택배 배송, Time: 348 ms, Memory: 40848 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.java
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -17,7 +16,6 @@ public class Main {
 		
 		for(int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
@@ -26,43 +24,43 @@ public class Main {
 			graph.get(b).add(new Node(a, c));
 		}
 		
-		int[] cost = new int[N + 1];
-		Arrays.fill(cost, Integer.MAX_VALUE);
-		cost[1] = 0; 
-
 		PriorityQueue<Node> pq = new PriorityQueue<>();
 		pq.offer(new Node(1, 0));
 		
+		int[] dist = new int[N + 1];
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		dist[1] = 0;
+		
 		while(!pq.isEmpty()) {
 			Node current = pq.poll();
+			int v = current.v;
+			int distance = current.distance;
 			
-			if(current.c > cost[current.v]) continue;
+			if(distance > dist[v]) continue;
 			
-			for(Node next: graph.get(current.v)) {
-				if(cost[current.v] + next.c < cost[next.v]) {
-					cost[next.v] = cost[current.v] + next.c;
-					pq.offer(new Node(next.v, cost[next.v]));
+			for(Node next: graph.get(v)) {
+				if(next.distance + dist[v] < dist[next.v]) {
+					dist[next.v] = next.distance + dist[v];
+					pq.offer(new Node(next.v, dist[next.v]));
 				}
 			}
 		}
-
-		System.out.println(cost[N]);
+		
+		System.out.println(dist[N]);
 	}
-
+	
 	static class Node implements Comparable<Node> {
 		int v;
-		int c;
+		int distance;
 		
-		public Node(int v, int c) {
+		public Node(int v, int distance) {
 			this.v = v;
-			this.c = c;
+			this.distance = distance;
 		}
 
 		@Override
 		public int compareTo(Node o) {
-			return Integer.compare(this.c, o.c);
+			return Integer.compare(this.distance, o.distance);
 		}
-		
-		
 	}
 }
