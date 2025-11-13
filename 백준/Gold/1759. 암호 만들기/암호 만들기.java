c4ab6c308695be6abd6cbd6a2ae27a982f5e15fb
영서 <116789976+youngseo3@users.noreply.github.com>
2025-11-13 13:54:59 +0900

    [Gold V] Title: 암호 만들기, Time: 68 ms, Memory: 11604 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,75 +1,63 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
 	static int L, C;
-	static char[] aeiou = {'a', 'e', 'i', 'o', 'u'};
- 	static char[] arr;
+	static char[] pwd;
 	static char[] selected;
-
 	static StringBuilder sb = new StringBuilder();
 	
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        L = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
-        
-        arr = new char[C];
-        selected = new char[L];
-        
-        st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < C; i++) {
-        	arr[i] = st.nextToken().charAt(0);
-        }
-        
-        Arrays.sort(arr);
-        
-        dfs(0, 0);
-        
-        System.out.println(sb);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		L = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		pwd = new char[C];
+		selected = new char[L];
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < C; i++) {
+			pwd[i] = st.nextToken().charAt(0);
+		}
+		
+		Arrays.sort(pwd);
+		
+		solve(0, 0);
+		System.out.println(sb);
 	}
 
-    static void dfs(int start, int depth) {
-    	if(depth == L) {
-    		if(isCode()) {
-    			for(char c: selected) {
-        			sb.append(c);
-        		}
-        		sb.append("\n");
-    		}
- 
-    		return;
-    	}
-    	
-    	for(int i = start; i < C; i++) {
-    		selected[depth] = arr[i];
-    		dfs(i + 1, depth + 1);
-    	}
-    }
-    
-    static boolean isCode() {
-    	int aeiouCnt = 0;
-    	
-    	for(char s: selected) {
-    		for(char c: aeiou) {
-        		if(c == s) {
-        			aeiouCnt++;
-        		}
-        	}
-    	}    	
-    	
-    	int remainCnt = L - aeiouCnt;
-    	
-    	if(remainCnt >= 2 && aeiouCnt >= 1) {
-    		return true;
-    	} else {
-    		return false;
-    	}
-    }
+	private static void solve(int depth, int start) {
+		if(depth == L) {
+			if(isValid()) {
+				for(int i = 0; i < selected.length; i++) {
+					sb.append(selected[i]);
+				}
+				sb.append("\n");
+			}
+			
+			return;
+		}
+		
+		for(int i = start; i < C; i++) {
+			selected[depth] = pwd[i];
+			solve(depth + 1, i + 1);
+		}
+	}
+	
+	private static boolean isValid() {
+		int vowelCount = 0; // 모음 개수
+		int consonantCount = 0; // 자음 개수
+		
+		for(char c : selected) {
+			if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+				vowelCount++;
+			} else {
+				consonantCount++;
+			}
+		}
+		
+		// "최소 한 개의 모음"과 "최소 두 개의 자음" 조건을 만족하는지 반환
+		return vowelCount >= 1 && consonantCount >= 2;
+	}
 }
