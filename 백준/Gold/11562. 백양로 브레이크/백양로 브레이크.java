d64de6605bf6c2535b4e01e28edaca9ac0925038
영서 <116789976+youngseo3@users.noreply.github.com>
2025-11-14 10:02:26 +0900

    [Gold III] Title: 백양로 브레이크, Time: 312 ms, Memory: 33016 KB -BaekjoonHub

---
+++ 백준/Gold/11562. 백양로 브레이크/백양로 브레이크.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static final int INF = 987654321;
+	
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[][] dist = new int[N + 1][N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (i != j) {
+                	dist[i][j] = INF;
+                }
+            }
+        }
+		
+		for(int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			dist[u][v] = 0;
+			if(b == 0) { // 일방통행 
+				dist[v][u] = 1;
+			} else {
+				dist[v][u] = 0;
+			}
+		}
+		
+		for(int k = 1; k <= N; k++) {
+			for(int i = 1; i <= N; i++) {
+				for(int j = 1; j <= N; j++) {
+					dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+				}
+			}
+		}
+		
+		int K = Integer.parseInt(br.readLine());
+		for(int i = 0; i < K; i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			
+			sb.append(dist[s][e] + "\n");
+		}
+		
+		System.out.println(sb);
+	}
+
+}
