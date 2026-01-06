39d98e1576ea6abaf0d2e9dbbe2f1bd77ac306a8
Youngseo Won <116789976+youngseo3@users.noreply.github.com>
2026-01-06 15:56:07 +0900

    [Gold V] Title: 적록색약, Time: 68 ms, Memory: 12352 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,78 +1,91 @@
+import java.awt.*;
 import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int N;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static char[][] arr;
+    static boolean[][] visited;
 
-	static int N, cnt;
-	static int[] dx = {0, 0, -1, 1};
-	static int[] dy = {-1, 1, 0, 0};
-	static char[][] map;
-	static boolean[][] visited;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		map = new char[N][N];
-		visited = new boolean[N][N];
-		
-		for(int i = 0; i < N; i++) {
-			map[i] = br.readLine().toCharArray();
-		}
-		
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				if(!visited[i][j]) {
-					bfs(i, j, map[i][j]);
-				}
-			}
-		}
-		System.out.print(cnt + " ");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		cnt = 0;
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				visited[i][j] = false;
-				
-				if(map[i][j] == 'R') {
-					map[i][j] = 'G';
-				}
-			}
-		}
-		
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < N; j++) {
-				if(!visited[i][j]) {
-					bfs(i, j, map[i][j]);
-				}
-			}
-		}
-		
-		System.out.println(cnt);
+        N = Integer.parseInt(br.readLine());
+        arr = new char[N][N];
+        visited = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = br.readLine().toCharArray();
+        }
 
-	}
+        int cnt = 0;
 
-	static void bfs(int x, int y, char firstColor) {
-		Queue<int[]> q = new LinkedList<>();
-		visited[x][y] = true;
-		q.add(new int[] {x, y});
-		
-		while(!q.isEmpty()) {
-			int[] current = q.poll();
-			
-			for(int i = 0; i < 4; i++) {
-				int nx = current[0] + dx[i];
-				int ny = current[1] + dy[i];
-				
-				if(nx >= 0 && nx < N && 
-						ny >= 0 && ny < N &&
-						!visited[nx][ny] &&
-						map[nx][ny] == firstColor) {
-					visited[nx][ny] = true;
-					q.add(new int[] {nx, ny});
-				}
-			}
-		}
-		
-		cnt++;
-	}
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j]) {
+                    cnt += bfs(i, j);
+                }
+            }
+        }
+
+        System.out.print(cnt + " ");
+
+        visited = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (arr[i][j] == 'R') {
+                    arr[i][j] = 'G';
+                }
+            }
+        }
+        cnt = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (!visited[i][j]) {
+                    cnt += bfs(i, j);
+                }
+            }
+        }
+
+        System.out.print(cnt);
+    }
+
+    private static int bfs(int x, int y) {
+        Queue<Point> q = new ArrayDeque<>();
+        q.add(new Point(x, y, arr[x][y]));
+
+        visited[x][y] = true;
+
+        while (!q.isEmpty()) {
+            Point p = q.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int nx = p.x + dx[i];
+                int ny = p.y + dy[i];
+
+                if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+                if (visited[nx][ny]) continue;
+                if (arr[nx][ny] != p.color) continue;
+
+                visited[nx][ny] = true;
+                q.add(new Point(nx, ny, arr[nx][ny]));
+            }
+        }
+
+        return 1;
+    }
+
+    static class Point {
+        int x;
+        int y;
+        int color;
+
+        public Point(int x, int y, int color) {
+            this.x = x;
+            this.y = y;
+            this.color = color;
+        }
+    }
 }
